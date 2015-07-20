8c2c2bad72e155a6bbb1474db4771a9d46342a9f
NicolasDelille <nicolas.delille@gmail.com>
2015-07-20 13:05:52 +0200

    DataBase connnexion setup

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
     define("DBHOST", "localhost"); //ip du serveur
     define("DBUSER", "root"); // username MySQL
     define("DBPASS", ""); // mot de passe MySQL
-    define("DBNAME", "survey"); //nom de la base de données (bdd)
+    define("DBNAME", "auth"); //nom de la base de données (bdd)
 
 	try {
 		$dbh = new PDO(
