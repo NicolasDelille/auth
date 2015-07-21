4ad8da227dba6326f2a624b88782440c56b92d35
NicolasDelille <nicolas.delille@gmail.com>
2015-07-21 09:59:05 +0200

    remove db connexion information from version

---
+++ config.dist.php
@@ -0,0 +1,5 @@
+<?php
+    define("DBHOST", "localhost"); //ip du serveur
+    define("DBUSER", "root"); // username MySQL
+    define("DBPASS", ""); // mot de passe MySQL
+    define("DBNAME", "auth"); //nom de la base de données (bdd)
